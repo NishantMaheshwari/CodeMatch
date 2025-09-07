251f47ca35853e1ad753dd3622a41de11da354f9
Nishant Maheshwari <nishant.maheswari@knolskape.com>
2025-09-07 17:12:08 +0530

    removed the synchronous start of server

--- server/app.js
+++ server/app.js
@@ -13,9 +13,9 @@ app.use(
   })
 );
 
-app.listen(3003,()=> {
-  console.log('Server has started');
-})
+// app.listen(3003, () => {
+//   console.log('Server has started');
+// })
 
 app.use(express.json());
 app.use(cookieParser());
