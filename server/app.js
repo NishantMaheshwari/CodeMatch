d4490e56840889713acc921b85eaefb22ff4cc35
Nishant Maheshwari <nishant.maheswari@knolskape.com>
2025-09-13 13:16:40 +0530

    added the update profile controllers

--- server/app.js
+++ server/app.js
@@ -20,16 +20,12 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-//routes
-// const authRouter = require("./src/routes/auth");
-// const profileRouter = require("./src/routes/profile");
-// const requestRouter = require("./src/routes/request");
-// const userRouter = require("./src/routes/user");
+// routes
+const authRouter = require("./src/routes/auth");
+const profileRouter = require("./src/routes/profile");
 
-// app.use("/", authRouter);
-// app.use("/", profileRouter);
-// app.use("/", requestRouter);
-// app.use("/", userRouter);
+app.use("/", authRouter);
+app.use("/", profileRouter);
 
 //database connect before server
 connectDB().then(() => {
