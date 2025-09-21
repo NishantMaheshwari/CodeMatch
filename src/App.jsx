308b5f7dece0fc62c1c96d3617045a8c35252185
Nishant Maheshwari <nishant.maheswari@knolskape.com>
2025-09-21 13:56:24 +0530

    added profile and feed pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Body from "./Components/Body";
 import Login from "./Components/Login";
+import Profile from "./Components/Profile";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
+import Feed from "./Components/Feed";
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
         <BrowserRouter basename="/">
           <Routes>
             <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
             </Route>
           </Routes>
         </BrowserRouter>
