f7f25acb22a7edbfbdd3d66ae76250b15d38f609
Nishant Maheshwari <nishant.maheswari@knolskape.com>
2025-09-27 14:11:04 +0530

    completed the chat interface

--- server/app.js
+++ server/app.js
@@ -28,12 +28,14 @@ const requestRouter = require("./src/routes/request");
 const userRouter = require("./src/routes/user");
 const paymentRouter = require("./routes/payment");
 const initializeSocket = require("./src/utils/socket");
+const chatRouter = require("./src/routes/chat");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 app.use("/", paymentRouter);
+app.use("/", chatRouter);
 
 const server = http.createServer(app);
 initializeSocket(server);
